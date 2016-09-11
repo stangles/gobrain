4f90c98e60a5a095957637ee34a3ac2014663ab6
Stangles <angles.steven@gmail.com>
2016-09-10 20:01:19 -0400

    read from stdin, remove debug statements

--- interpreter.go
+++ interpreter.go
@@ -1,63 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
+// 30000 bytes according to brainfuck "spec"
 var data [30000]byte
 
 func main() {
 	input := "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
 	dataPointer := 0
 
+	reader := bufio.NewReader(os.Stdin)
+
 	prevOpenLoopIdx := -1
-	// prevCloseLoopIdx := -1
 	for i := 0; i < len(input); i++ {
 		switch input[i] {
 		case '>':
-			// fmt.Printf("data pointer was: %v, now: %v\n", dataPointer, dataPointer+1)
 			dataPointer++
 		case '<':
-			// fmt.Printf("data pointer was: %v, now: %v\n", dataPointer, dataPointer-1)
 			dataPointer--
 		case '+':
 			data[dataPointer]++
-			// fmt.Printf("incrementing cell at %v, value now %v\n", dataPointer, data[dataPointer])
 		case '-':
 			data[dataPointer]--
-			// fmt.Printf("decrementing cell at %v, value now %v\n", dataPointer, data[dataPointer])
 		case '.':
-			// fmt.Printf("printing cell at %v\n", dataPointer)
 			fmt.Printf("%v", string(data[dataPointer]))
 		case ',':
-			// TODO accept single byte of input
+			b, err := reader.ReadByte()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			data[dataPointer] = b
 		case '[':
 			if data[dataPointer] != 0 {
-				// fmt.Printf("entering loop, data pointer: %v\n", dataPointer)
 				prevOpenLoopIdx = i
 				continue
 			} else {
 				// set ip to instruction after next ']'
-				// fmt.Printf("exiting loop, data pointer: %v\n", dataPointer)
-				afterClose := strings.IndexByte(string(input[i:]), ']') + 1
-				if afterClose == 0 {
+				afterClose := strings.IndexByte(string(input[i:]), ']')
+				if afterClose == -1 {
 					panic(fmt.Sprintf("Unterminated loop caught beginning at idx: %v", i))
 				}
 				prevOpenLoopIdx = -1
-				i = afterClose - 1
+				i = afterClose
 			}
 		case ']':
 			if data[dataPointer] != 0 {
 				// set ip to instruction after previous '['
 				if prevOpenLoopIdx != -1 {
-					// fmt.Printf("looping, data pointer: %v\n", dataPointer)
 					i = prevOpenLoopIdx
 				} else {
 					panic(fmt.Sprintf("Encountered loop termination without opening bracket at idx: %v", i))
 				}
 			} else {
-				// fmt.Printf("exiting loop, data pointer: %v\n", dataPointer)
 				prevOpenLoopIdx = -1
 				continue
 			}
